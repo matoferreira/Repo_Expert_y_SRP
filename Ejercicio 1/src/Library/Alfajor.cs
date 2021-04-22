063a4f1b5742dfb4ef6fbfc4ce7659635fa0b717
matoferreira <matifer1@gmail.com>
2021-04-22 20:40:56 -0300

    Agregado justificación en comentarios, en base a SRP y Expert

--- Ejercicio 1/src/Library/Alfajor.cs
+++ Ejercicio 1/src/Library/Alfajor.cs
@@ -9,7 +9,15 @@ namespace Expert_SRP
             this.PrecioDulce = precioDulce;
             this.PrecioMasa = precioMasa;
         }
+        ///Falta un método que calcule el precio final del alfajor sumando las variables del precio de sus ingredientes
         public Double PrecioDulce {get;set;}
         public Double PrecioMasa {get; set;}
     }
 }
+/* 
+No cumple con el patrón de Expert porque la clase Alfajor no es la responsable de calcular el 
+precio final del Alfajor. 
+Una vez se agregue el método de calcular el precio final, cumplirá con el patrón de Expert y con el
+principio de SRP porque su responsabilidad es calcular el precio final y su única razón de cambio es la
+variación del precio de los ingredientes.
+*/
--- Ejercicio 1/src/Library/Kiosco.cs
+++ Ejercicio 1/src/Library/Kiosco.cs
@@ -7,8 +7,15 @@ namespace Expert_SRP
         public Boolean PuedeComprar(Alfajor a, Double dinero, String moneda) 
         {
             Double pesos = ConvertirAPesos(dinero, moneda);
-            return pesos >= a.PrecioDulce + a.PrecioMasa;
+            return pesos >= a.PrecioDulce + a.PrecioMasa; /* Esto no cumple el patrón Expert porque:
+            El cálculo lo debería realizar el Alfajor (al calculo final del precio) 
+            porque es el experto en la información del valor de los ingredientes del mismo.*/
         }
+
+/* Debería existir una clase de Conversión de moneda ya que:
+No cumple con el principio de SRP debido a que Kiosco es responsable tanto de Comprobar si se puede
+comprar el producto, como de hacer la conversión de la moneda. Por ende, se debe crear una clase que sea
+responsable de hacer la conversión y que colabore con Kiosco. */
         private Double ConvertirAPesos(Double dinero, String moneda)
         {
             if (moneda.Equals("U$S")) 
