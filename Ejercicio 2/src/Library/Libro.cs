2872f75b3960f3d59396ed070e7a456f9b2c819c
matoferreira <matifer1@gmail.com>
2021-04-27 09:31:42 -0300

    Agregado Justificación al EJ2 según Principio Expert

--- Ejercicio 2/src/Library/Libro.cs
+++ Ejercicio 2/src/Library/Libro.cs
@@ -18,6 +18,15 @@ namespace SRP
             this.Codigo = codigo;
         }
 
+/* A continuación podemos ver como no se cumple el Principio SRP
+Esto se debe a que, para cumplirlo, cada clase debe tener una única responsabilidad,
+una única razón de cambio.
+La clase Libro tiene dos responsabilidades, la primera es ser el constructor de los libros,
+la segunda es almacenar los libros en un sector de la biblioteca.
+Si nosotros quisieramos cambiar la forma en la que se almacenan los libros ahora, tendríamos que modificar la
+clase Libro, de igual manera si necesitaramos cambiar la forma en la que se crean los libros.
+Para cumplir con este principio es necesario crear una clase nueva cuya responsabilidad sea la de Almacenar los libros.
+*/
         public void AlmacenarLibro(String sector, String estante)
         {
             this.SectorBiblioteca = sector;
