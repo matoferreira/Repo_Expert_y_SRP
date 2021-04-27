10c13a5a0baf372f4994c8685581f90fd283feef
matoferreira <matifer1@gmail.com>
2021-04-27 10:42:01 -0300

    Agregado validación a la clase Doctor

--- Ejercicio 3/src/Library/Doctor.cs
+++ Ejercicio 3/src/Library/Doctor.cs
@@ -4,14 +4,57 @@ namespace Library
 {
     public class Doctor
     {
-        public string DoctorName { get ; set; }
-        public string Especialidad { get ; set; }
+        Boolean isValid = true;
+        public string DoctorName
+        { 
+            get
+            {
+                return this.DoctorName;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value) == false)
+                {
+                    this.DoctorName = value;
+                }
+                else
+                {
+                    Console.WriteLine("El nombre que ingresó no es válido, por favor intente de nuevo");
+                    isValid = false;
+                }
+            }
+        }
+        public string Especialidad 
+        { 
+            get
+            {
+                return this.Especialidad;
+            } 
+            set
+            {
+                if (string.IsNullOrEmpty(value) == false)
+                {
+                    this.Especialidad = value;
+                }
+                else
+                {
+                    Console.WriteLine("La especialidad que ingresó no es válida, por favor intente de nuevo");
+                    isValid = false;
+                }
+            }
+        }
 
         public Doctor(String doctorname, String especialidad)
         {
-            this.DoctorName = doctorname;
-            this.Especialidad = especialidad;
-
+            if (isValid == true)
+            {
+                this.DoctorName = doctorname;
+                this.Especialidad = especialidad;
+            }
+            else
+            {
+                Console.WriteLine("No se pudo agregar el médico, por favor verifique e intente nuevamente");
+            }
         }
     }
 
