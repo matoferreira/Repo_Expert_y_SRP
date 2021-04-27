bcb208199f1b4d6c42f87a2b7792bdb401f765d2
matoferreira <matifer1@gmail.com>
2021-04-27 18:54:04 -0300

    Corrección de la sintaxis del código

--- Ejercicio 1/src/Library/Alfajor.cs
+++ Ejercicio 1/src/Library/Alfajor.cs
@@ -4,13 +4,14 @@ namespace Expert_SRP
 {
     public class Alfajor 
     {
+        public Double PrecioDulce {get;set;}
+        public Double PrecioMasa {get; set;}
         public Alfajor(double precioMasa, double precioDulce)
         {
             this.PrecioDulce = precioDulce;
             this.PrecioMasa = precioMasa;
         }
-        public Double PrecioDulce {get;set;}
-        public Double PrecioMasa {get; set;}
+       
 
         
         public Double PrecioAlfajor() {
